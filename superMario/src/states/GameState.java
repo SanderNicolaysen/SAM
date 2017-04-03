9e093434c2feaf9df98cf5db6a727ae56504717e
Sander Nicolaysen <sanden16@student.uia.no>
2017-04-04 01:34:44 +0200

    Added small animation

--- superMario/src/states/GameState.java
+++ superMario/src/states/GameState.java
@@ -15,19 +15,24 @@ public class GameState extends State
 
     }
     int x = 0;
-    int y = 0;
     @Override
     public void tick()
     {
-        x++;
-        y += 2;
+        x += 2;
     }
 
     @Override
     public void render(Graphics g)
     {
+        g.setColor(Color.CYAN.darker());
+        g.fillRect(0, 0, 900, 700);
+
+        g.setColor(Color.GREEN.darker());
+        g.fillRect(0, 600, 900, 100);
+
+        g.setColor(Color.RED.darker());
+        g.fillRect(x, 580, 20, 20);
+
         g.drawImage(Assets.ground, 0, 0, null);
-        g.setColor(Color.RED);
-        g.fillRect(x, 400, x, 50);
     }
 }
