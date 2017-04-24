b57dc6a5b1e7bf8645284f7ee974666fdfa86151
Marcus Korsnes Morlandstø <marcum16@student.uia.no>
2017-04-24 18:13:57 +0200

    Lagt til animasjonsklasse (uten sprite koordinater)

--- superMario/src/graphics/Assets.java
+++ superMario/src/graphics/Assets.java
@@ -12,11 +12,15 @@ public class Assets
 
     // Different tiles/images that will be equal to cropped out pictures of our sprite sheet.
 
-    public static BufferedImage marioRightNormal, marioRightMove1, marioRightMove2, marioRightMove3, marioRightJump,
-            marioLeftNormal, marioLeftMove1, marioLeftMove2, marioLeftMove3, marioLeftJump, marioDeath,
-            superMarioRightNormal, superMarioRightMove1, superMarioRightMove2, superMarioRightMove3, superMarioRightJump, superMarioRightCrouch,
-            superMarioLeftNormal, superMarioLeftMove1, superMarioLeftMove2, superMarioLeftMove3, superMarioLeftJump, superMarioLeftCrouch, ground,
-            bg;
+    public static BufferedImage marioRightNormal, marioRightJump, marioLeftNormal, marioLeftJump, marioDeath,
+            superMarioRightNormal, superMarioRightCrouch, superMarioRightJump, superMarioLeftNormal, superMarioLeftCrouch, superMarioLeftJump,
+            ground, bg;
+
+    public static BufferedImage[] marioRightMove;
+    public static BufferedImage[] marioLeftMove;
+    public static BufferedImage[] superMarioRightMove;
+    public static BufferedImage[] superMarioLeftMove;
+
 
     /**
      * This method will load in all the assets we need in our game.
@@ -33,31 +37,39 @@ public class Assets
 
         //Mario's right movements
         marioRightNormal = superMarioSheet.crop((width * 14) - 1, 0, width, height);
-        marioRightMove1 = superMarioSheet.crop((width * 16), 0, width + 4, height);
-        marioRightMove2 = superMarioSheet.crop(0, 0, width, height);
-        marioRightMove3 = superMarioSheet.crop(0, 0, width, height);
         marioRightJump = superMarioSheet.crop(0, 0, width, height);
+        marioRightMove = new BufferedImage[2];
+        marioRightMove[0] = superMarioSheet.crop(0, 0, width, height);
+        marioRightMove[1] = superMarioSheet.crop(0, 0, width, height);
+        marioRightMove[2] = superMarioSheet.crop(0, 0, width, height);
+
         //Mario's left movements
         marioLeftNormal = superMarioSheet.crop(0, 0, width, height);
-        marioLeftMove1 = superMarioSheet.crop(0, 0, width, height);
-        marioLeftMove2 = superMarioSheet.crop(0, 0, width, height);
-        marioLeftMove3 = superMarioSheet.crop(0, 0, width, height);
         marioLeftJump = superMarioSheet.crop(0, 0, width, height);
+        marioLeftMove = new BufferedImage[2];
+        marioLeftMove[0] = superMarioSheet.crop(0, 0, width, height);
+        marioLeftMove[1] = superMarioSheet.crop(0, 0, width, height);
+        marioLeftMove[2] = superMarioSheet.crop(0, 0, width, height);
+
         //Mario's death movement
         marioDeath = superMarioSheet.crop(0, 0, width, height);
+
         //Super Mario's right movements
         superMarioRightNormal = superMarioSheet.crop(0, 0, width, height);
-        superMarioRightMove1 = superMarioSheet.crop(0, 0, width, height);
-        superMarioRightMove2 = superMarioSheet.crop(0, 0, width, height);
-        superMarioRightMove3 = superMarioSheet.crop(0, 0, width, height);
         superMarioRightJump = superMarioSheet.crop(0, 0, width, height);
         superMarioRightCrouch = superMarioSheet.crop(0, 0, width, height);
+        superMarioRightMove = new BufferedImage[2];
+        superMarioRightMove[0] = superMarioSheet.crop(0, 0, width, height);
+        superMarioRightMove[1] = superMarioSheet.crop(0, 0, width, height);
+        superMarioRightMove[2] = superMarioSheet.crop(0, 0, width, height);
+
         //Super Mario's left movements
         superMarioLeftNormal = superMarioSheet.crop(0, 0, width, height);
-        superMarioLeftMove1 = superMarioSheet.crop(0, 0, width, height);
-        superMarioLeftMove2 = superMarioSheet.crop(0, 0, width, height);
-        superMarioLeftMove3 = superMarioSheet.crop(0, 0, width, height);
         superMarioLeftJump = superMarioSheet.crop(0, 0, width, height);
         superMarioLeftCrouch = superMarioSheet.crop(0, 0, width, height);
+        superMarioLeftMove = new BufferedImage[2];
+        superMarioLeftMove[0] = superMarioSheet.crop(0, 0, width, height);
+        superMarioLeftMove[1] = superMarioSheet.crop(0, 0, width, height);
+        superMarioLeftMove[2] = superMarioSheet.crop(0, 0, width, height);
     }
 }
