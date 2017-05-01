ae48ae155cc3e28f682f60dbd0f45e6135ce0f3d
Pål Tømte Karlsen <paaltk16@student.uia.no>
2017-05-02 00:35:41 +0200

    Timer

--- superMario/src/states/GameOver.java
+++ superMario/src/states/GameOver.java
@@ -26,33 +26,29 @@ public class GameOver extends State {
 
     @Override
     public void tick() {
-        //handler.getWorld().getEntityManager().getEntities().clear();
-
-
+        //ein pause før den går tilbake til meny
+        
+        try {
+            Thread.sleep(3000);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
         State.setState(handler.getGame().menuState);
     }
+
 
     @Override
     public void render(Graphics g) {
 
-
-            //g.setColor(Color.BLACK);
-            //g.fillRect(0, 0, 1920, 1080);
-            //g.setFont(new Font("marioFont", Font.BOLD, 60));
-            //g.setColor(Color.WHITE);
-            //g.drawString("GAME OVER", handler.getWidth() / 2 - 300, handler.getHeight() / 2);
-
-           /*
-            g.drawString("GAME OVER", handler.getWidth() / 2 - 300, handler.getHeight() / 2);
-            Timer = 3000;
-            State.setState(handler.getGame().menuState);
-            */
-        /*
+        //gameover screen with points
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, 1920, 1080);
         g.setFont(new Font("marioFont", Font.BOLD, 60));
         g.setColor(Color.WHITE);
-        g.drawString("GAME OVER", handler.getWidth() / 2 - 300, handler.getHeight() / 2);
-        */
+        g.drawString("GAME OVER", handler.getWidth() / 2 - 300, handler.getHeight() / 2 - 100);
+        g.setFont(new Font("marioFont", Font.BOLD, 30));
+        g.setColor(Color.WHITE);
+        g.drawString("Points: " + " ", handler.getWidth() / 2 - 300, handler.getHeight() / 2);
+
     }
 }
